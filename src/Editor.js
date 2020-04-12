bb5005bdb2f75be6a6b74fc7e1122f2efef24ea7
jihgao <jihgao@163.com>
2020-04-12 18:09:28 +0800

    Step2 introduce editor

--- src/Editor.js
+++ src/Editor.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useRef, useEffect } from 'react';
+import { editor } from "monaco-editor";
+import { debounce } from 'lodash';
 
-function Editor() {
+
+
+function Editor(props) {
+  const container = useRef(null);
+  const { value, onChange } = props;
+  useEffect(() => {
+    const _editor =  editor.create(container.current, {
+      value: value,
+      language: "markdown",
+      renderLineHighLight: "none",
+      lineDecorationWidth: 0,
+      lineNumbersLeft: 0,
+      lineNumbersWidth: 0,
+      fontSize: 20,
+      lineNumbers: "on",
+      automaticLayout: false,
+      quickSuggestions: false,
+      occurrencesHighlight: false,
+      colorDecorators: false,
+      wordWrap: true,
+      theme: "vs-dark",
+      minimap: {
+        enabled: false
+      }
+    });
+    const _model = _editor.getModel();
+    _model.onDidChangeContent(debounce(() => {
+      onChange(_model.getValue());
+    }, 500));
+  }, [value, onChange])
   return (
-    <div className="editor">editor</div>
+    <div className="editor" ref={container} />
   );
 }
+
+Editor.defaultProps = {
+  value: '',
+  onChange:() => { }
+};
 
 export default Editor;
