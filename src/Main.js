c71d7246d3de4c459fe2e9404ac3fa26ac380730
jihgao <jihgao@163.com>
2020-04-12 18:26:03 +0800

    Step3 preview

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Preview from "./Preview";
 import Editor from "./Editor";
 
 function Main() {
+  const [source, setSource] = useState('');
+  function handleSourceChange(newSource) {
+    setSource(newSource);
+  }
   return (
     <div className="main">
-      <Editor />
-      <Preview />
+      <Editor onChange={handleSourceChange}/>
+      <Preview source={source}/>
     </div>
   );
 }
--- src/Preview.js
+++ src/Preview.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useRef, useEffect} from "react";
+import MarkdownIt from "markdown-it";
+import tocPlugin from "markdown-it-table-of-contents";
 
-function Preview() {
+const md = new MarkdownIt({
+  html: false,
+  xhtmlOut: false,
+  breaks: false,
+  langPrefix: "language-",
+  linkify: true,
+  typographer: false,
+  quotes: "“”‘’"
+});
+md.use(tocPlugin, { includeLevel: [2, 3], markerPattern: /^\[toc\]/im });
+
+
+function Preview(props) {
+  const { source } = props;
+  const ele = useRef(null);
+  useEffect(() => {
+    ele.current.innerHTML = md.render(source || "");
+  }, [source]);
   return (
-    <div className="preview">preview</div>
+    <div className="preview" ref={ele}/>
   );
 }
 
